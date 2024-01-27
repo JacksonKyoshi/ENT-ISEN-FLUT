860930795b81cc1a21de1b2fa2363fe4d9f6489c
Pierre-Gei <pierregeiguer@gmail.com>
2024-01-27 02:47:22 +0100

    Hamburger menu V1

--- flut/lib/widgets/hamburger_menu.dart
+++ flut/lib/widgets/hamburger_menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HamburgerMenu extends StatelessWidget {
   const HamburgerMenu({super.key});
@@ -9,63 +10,97 @@ class HamburgerMenu extends StatelessWidget {
     return Drawer(
       child: ListView(
         children: [
-          DrawerHeader(
-            decoration: const BoxDecoration(
-              color: Colors.red,
-            ),
-            child: Column(
-              children: [
-                const Text(
-                  'ISEN Toulon',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
+          Container(
+            height: 200, // specify the height
+            width: 300, // specify the width
+            child: DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.red,
+              ),
+              child: Column(
+                children: [
+                  const Text(
+                    'ENT ISEN Toulon',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
                   ),
-                ),
-                Container(
-                  child: Image.asset('lib/assets/ISEN-YNCREA-Mediterranee-White.png', fit: BoxFit.cover),
-                ),
-              ],
+                  Container(
+                    child: Image.asset('lib/assets/ISEN-YNCREA-Mediterranee-White.png', fit: BoxFit.cover),
+                  ),
+                ],
+              ),
             ),
           ),
           ListTile(
+            leading: SvgPicture.asset(
+              'lib/assets/SVG/house-solid.svg',
+              height: 24,
+              width: 24,
+              color: Colors.black,
+            ),
             title: const Text('Accueil'),
             onTap: () {
               Navigator.pop(context);
             },
           ),
           ListTile(
-            title: const Text('Actualités'),
+            leading: SvgPicture.asset(
+              'lib/assets/SVG/graduation-cap-solid.svg',
+              height: 24,
+              width: 24,
+              color: Colors.black,
+            ),
+            title: const Text('Notes'),
             onTap: () {
               Navigator.pop(context);
             },
           ),
           ListTile(
-            title: const Text('Agenda'),
+            leading: SvgPicture.asset(
+              'lib/assets/SVG/calendar-days-solid.svg',
+              height: 24,
+              width: 24,
+              color: Colors.black,
+            ),
+            title: const Text('Emploi du temps'),
             onTap: () {
               Navigator.pop(context);
             },
           ),
           ListTile(
-            title: const Text('Annuaire'),
+            leading: SvgPicture.asset(
+              'lib/assets/SVG/user-large-slash-solid.svg',
+              height: 24,
+              width: 24,
+              color: Colors.black,
+            ),
+            title: const Text('Absences'),
             onTap: () {
               Navigator.pop(context);
             },
           ),
           ListTile(
-            title: const Text('Moodle'),
+            leading: SvgPicture.asset(
+              'lib/assets/SVG/book-solid.svg',
+              height: 24,
+              width: 24,
+              color: Colors.black,
+            ),
+            title: const Text('EDUC'),
             onTap: () {
               Navigator.pop(context);
             },
           ),
           ListTile(
-            title: const Text('Intranet'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: const Text('Contact'),
+            leading: SvgPicture.asset(
+              'lib/assets/SVG/globe-solid.svg',
+              height: 24,
+              width: 24,
+              color: Colors.black,
+            ),
+            title: const Text('Site web'),
             onTap: () {
               Navigator.pop(context);
             },
