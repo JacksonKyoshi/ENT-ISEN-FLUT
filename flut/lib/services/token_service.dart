6c1f3c001981274db4805bf04666d2b98f36f771
bastientt <bastienschneider45@gmail.com>
2024-01-30 08:59:58 +0100

    feat : implement class tokenManager

---
+++ flut/lib/services/token_service.dart
@@ -0,0 +1,22 @@
+
+//Classe qui va gérer le token
+class TokenManager {
+  static TokenManager _instance = TokenManager._internal();
+  late String _token;
+  TokenManager._internal();
+
+
+  static TokenManager getInstance() {
+    return _instance;
+  }
+
+  void setToken(String token) { //setter
+    _token = token;
+  }
+
+
+  String getToken() { //getter
+    return _token;
+  }
+}
+
