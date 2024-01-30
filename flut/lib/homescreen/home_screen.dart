011b9845dce165f9b72fa5d0bcfc995f63989391
Pierre-Gei <pierregeiguer@gmail.com>
2024-01-30 15:22:54 +0100

    Progressbar

--- flut/lib/homescreen/home_screen.dart
+++ flut/lib/homescreen/home_screen.dart
@@ -8,6 +8,7 @@ import '../notesscreen/note_screen.dart';
 import '../services/api_service.dart';
 import '../widgets/hamburger_menu.dart';
 import 'dart:async';
+import '../services/ics_parser.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -51,9 +52,23 @@ class _HomeScreenState extends State<HomeScreen> {
     super.dispose();
   }
 
+  double calculateProgress(CalendarEvent event) {
+    DateTime now = DateTime.now();
+    double totalDuration =
+        event.end!.difference(event.start!).inMinutes.toDouble();
+    double elapsedDuration = now.difference(event.start!).inMinutes.toDouble();
+    return elapsedDuration / totalDuration;
+  }
+
   @override
   Widget build(BuildContext context) {
     var calendarEvents = Provider.of<CalendarEventProvider>(context).events;
+    CalendarEvent? currentEvent;
+    if (calendarEvents != null && calendarEvents.isNotEmpty) {
+      currentEvent = calendarEvents.firstWhere((event) =>
+          event.start!.isBefore(DateTime.now()) &&
+          event.end!.isAfter(DateTime.now()));
+    }
     return Scaffold(
       appBar: AppBar(
         //white text color
@@ -62,7 +77,8 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.red,
       ),
       drawer: const HamburgerMenu(),
-      body: SingleChildScrollView( // Add this
+      body: SingleChildScrollView(
+        // Add this
         child: Center(
           child: Container(
             child: Column(
@@ -86,19 +102,18 @@ class _HomeScreenState extends State<HomeScreen> {
                     future: _notationsFuture,
                     builder: (context, snapshot) {
                       if (snapshot.hasData) {
-                        return Container(
-                          width: MediaQuery.of(context).size.width - 30,
-                          padding: const EdgeInsets.all(20),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            color: Colors.blue.shade100,
-                          ),
-                          child: Column(
-                            children: [
-                              Text(snapshot.data![0].code),
-                              Text(snapshot.data![0].note.toString(),
-                                  style: const TextStyle(fontSize: 30)),
-                            ],
+                        return Card(
+                          // Wrap the Container with a Card
+                          child: Container(
+                            width: MediaQuery.of(context).size.width - 30,
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              children: [
+                                Text(snapshot.data![0].code),
+                                Text(snapshot.data![0].note.toString(),
+                                    style: const TextStyle(fontSize: 30)),
+                              ],
+                            ),
                           ),
                         );
                       } else if (snapshot.hasError) {
@@ -112,8 +127,8 @@ class _HomeScreenState extends State<HomeScreen> {
                   padding: const EdgeInsets.all(20),
                   child: const Align(
                     alignment: Alignment.topLeft,
-                    child:
-                    Text('Prochains Cours :', style: TextStyle(fontSize: 20)),
+                    child: Text('Prochains Cours :',
+                        style: TextStyle(fontSize: 20)),
                   ),
                 ),
                 InkWell(
@@ -126,45 +141,95 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Column(
                     children: calendarEvents != null
                         ? calendarEvents!
-                        .where((event) => event.start!.isBefore(DateTime.now()) && event.end!.isAfter(DateTime.now()) || event.start!.isAfter(DateTime.now()))
-                        .take(2)
-                        .map((event) => Card(
-                      child: ListTile(
-                        leading: Icon(
-                          event.summary!.contains('PROJET') ? Icons.bar_chart :
-                          event.summary!.contains('TD') ? Icons.calculate :
-                          event.summary!.contains('TP') ? Icons.memory :
-                          event.summary!.contains('CM') ? Icons.mic :
-                          event.summary!.contains('DS') ? Icons.school :
-                          event.summary!.contains('EXAMEN') ? Icons.school :
-                          event.summary!.contains('RATTRAPAGE') ? Icons.school :
-                          event.summary!.contains('REUNION') ? Icons.people :
-                          Icons.event,
-                        ),
-                        title: Container(
-                          width: MediaQuery.of(context).size.width - 30,
-                          padding: const EdgeInsets.all(20),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '${DateFormat('HH:mm').format(event.start!)} - ${DateFormat('HH:mm').format(event.end!)}',
-                                textAlign: TextAlign.left,
-                                style: Theme.of(context).textTheme.titleLarge,
-                              ),
-                              Text('${event.summary}',
-                                textAlign: TextAlign.left,
-                                style: Theme.of(context).textTheme.titleMedium,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ))
-                        .toList()
+                            .where((event) =>
+                                event.start!.isBefore(DateTime.now()) &&
+                                    event.end!.isAfter(DateTime.now()) ||
+                                event.start!.isAfter(DateTime.now()))
+                            .take(2)
+                            .map((event) => Card(
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width -
+                                        30, // Set the width of the Container
+                                    child: Stack(
+                                      alignment: Alignment.center,
+                                      children: [
+                                        ListTile(
+                                          leading: Icon(
+                                            event.summary!.contains('PROJET')
+                                                ? Icons.bar_chart
+                                                : event.summary!.contains('TD')
+                                                    ? Icons.calculate
+                                                    : event.summary!
+                                                            .contains('TP')
+                                                        ? Icons.memory
+                                                        : event.summary!
+                                                                .contains('CM')
+                                                            ? Icons.mic
+                                                            : event.summary!
+                                                                    .contains(
+                                                                        'DS')
+                                                                ? Icons.school
+                                                                : event.summary!
+                                                                        .contains(
+                                                                            'EXAMEN')
+                                                                    ? Icons
+                                                                        .school
+                                                                    : event.summary!.contains(
+                                                                            'RATTRAPAGE')
+                                                                        ? Icons
+                                                                            .school
+                                                                        : event.summary!.contains('REUNION')
+                                                                            ? Icons.people
+                                                                            : Icons.event,
+                                          ),
+                                          title: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                '${DateFormat('HH:mm').format(event.start!)} - ${DateFormat('HH:mm').format(event.end!)}',
+                                                textAlign: TextAlign.left,
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .titleLarge,
+                                              ),
+                                              Text(
+                                                '${event.summary}',
+                                                textAlign: TextAlign.left,
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .titleMedium,
+                                              ),
+                                              //if its the frist tile display the progress bar
+                                              if (event ==
+                                                  calendarEvents!.firstWhere(
+                                                      (event) =>
+                                                          event.start!.isBefore(
+                                                              DateTime.now()) &&
+                                                          event.end!.isAfter(
+                                                              DateTime.now())))
+                                                LinearProgressIndicator(
+                                                  value:
+                                                      calculateProgress(event),
+                                                  backgroundColor:
+                                                      Colors.grey.shade400,
+                                                  valueColor:
+                                                      AlwaysStoppedAnimation<Color>(Colors.red),
+                                                  minHeight: 10,
+                                                  borderRadius:
+                                                      BorderRadius.circular(10),
+                                                ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ))
+                            .toList()
                         : [const CircularProgressIndicator()],
                   ),
-                )
+                ),
               ],
             ),
           ),
