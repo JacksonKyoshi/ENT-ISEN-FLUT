34a0bec92ddb5c4e2fa6317df8a12a846ac27594
bastientt <bastienschneider45@gmail.com>
2024-01-30 08:29:21 +0100

    feat : Add loginscreen view.

--- flut/lib/main.dart
+++ flut/lib/main.dart
@@ -1,15 +1,22 @@
 // main.dart
 import 'package:flutter/material.dart';
 import 'homescreen/home_screen.dart';
+import 'loginscreen/LoginScreen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(LoginApp());
 }
 //test push
 class MyApp extends StatelessWidget {
+  String TOKEN;
+  String username;
+
+  MyApp(this.TOKEN,this.username);
+
 
   @override
   Widget build(BuildContext context) {
+    print('OMG LE CUL LE TOKEN DE MERDE: $TOKEN');
     return MaterialApp(
       title: 'ENT ISEN MOODLE',
       theme: ThemeData(
@@ -19,4 +26,5 @@ class MyApp extends StatelessWidget {
       home: const HomeScreen(),
     );
   }
+
 }
