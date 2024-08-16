31061823742fe037443e90d3888a160476250a37
Sigeth <52868615+Sigeth@users.noreply.github.com>
2024-08-16 20:49:53 +0200

    refactor: bottom nav and appbar theme has been moved to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,32 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'ENT ISEN Toulon',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
-          primarySwatch: Colors.red,
-          splashColor: const Color(0x00000000)
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+          primarySwatch: Colors.purple,
+          splashColor: const Color(0x00000000),
+          appBarTheme: const AppBarTheme(
+            foregroundColor: Color(0xFFFFFFFF),
+            backgroundColor: Color(0xFF2A2450),
+          ),
+          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+            backgroundColor: Color(0xFF2A2450),
+            selectedLabelStyle: TextStyle(
+              fontSize: 14,
+            ),
+            selectedItemColor: Color(0xFFFFFFFF),
+            selectedIconTheme: IconThemeData(
+                size: 32
+            ),
+            unselectedLabelStyle: TextStyle(
+              fontSize: 14,
+            ),
+            unselectedItemColor: Color(0xFF888888),
+            unselectedIconTheme: IconThemeData(
+                size: 24
+            ),
+            showUnselectedLabels: true,
+          )
+
         ),
         home: MainHandler(),
       ),
