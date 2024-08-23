9630cfa7a32684dd050411dcfd75ce5587d9b389
Sigeth <52868615+Sigeth@users.noreply.github.com>
2024-08-24 00:01:29 +0200

    feat: v1 of reworked login screen
    we need to add a background image, to implement the 'Remember me' checkbox and to create Mentions Légales screen

--- lib/loginscreen/login_screen.dart
+++ lib/loginscreen/login_screen.dart
@@ -41,139 +41,154 @@ class _LoginPageState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: MediaQuery.of(context).size.height / 10),
-              Text(
-                  "Flut",
-                  style: TextStyle(
-                      fontSize: 42
-                  )
-              ),
-              Text(
-                  "ENT ISEN",
-                  style: TextStyle(
-                      fontSize: 42
-                  )
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height / 10),
-              Container( // form container
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                        left: MediaQuery.of(context).size.width / 16, right: MediaQuery.of(context).size.width / 16
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        TextFormField(
-                          controller: _usernameController,
-                          autocorrect: false,
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.person, size: 32),
-                              labelText: "Identifiant",
-                              labelStyle: TextStyle(color: Colors.grey),
-                              floatingLabelStyle: TextStyle(color: Colors.purple, fontSize: 16),
-                              hintText: "prenom.nom",
-                              hintStyle: TextStyle(color: Colors.grey, fontSize: 12),
-                              enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.purple
-                                  )
-                              )
-                          ),
-                      ),
-                      SizedBox(height: MediaQuery.of(context).size.height / 32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+                "Flut\nENT ISEN",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 36,
+                )
+            ),
+            Container( // form container
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width / 16, right: MediaQuery.of(context).size.width / 16
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
                       TextFormField(
-                        controller: _passwordController,
-                        obscureText: _isPasswordHidden,
-                        obscuringCharacter: "●",
+                        controller: _usernameController,
                         autocorrect: false,
-                        decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.lock, size: 32),
-                            suffixIcon: IconButton(
-                                icon: Icon(_isPasswordHidden ? Icons.visibility : Icons.visibility_off, size: 20),
-                                onPressed: () {
-                                  setState(() {
-                                    _isPasswordHidden = !_isPasswordHidden;
-                                  });
-                                },
-                            ),
-                            labelText: "Mot de passe",
+                        decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.person, size: 48),
+                            labelText: "Identifiant",
                             labelStyle: TextStyle(color: Colors.grey),
-                            floatingLabelStyle: TextStyle(color: Colors.purple, fontSize: 16),
-                            hintText: "●●●●●●",
-                            hintStyle: TextStyle(color: Colors.grey, fontSize: 12),
+                            floatingLabelStyle: TextStyle(color: Colors.purple, fontSize: 20),
+                            hintText: "prenom.nom",
+                            hintStyle: TextStyle(color: Colors.grey, fontSize: 16),
                             enabledBorder: UnderlineInputBorder(
                                 borderSide: BorderSide(
                                     color: Colors.purple
                                 )
                             )
                         ),
-                      ),
-                      Row(
-                        children: [
-                          Text("Se souvenir de moi"),
-                          Checkbox(value: _remindOfMe, onChanged: (changedState) {
-                            setState(() {
-                              _remindOfMe = changedState!;
-                            });
-                          })
-                        ]
-                      ),
-                      ElevatedButton(
-                          onPressed: () async {
-                            String username = _usernameController.text;
-                            String password = _passwordController.text;
-                            const url = 'https://api-ent.isenengineering.fr/v1/token';
-
-                            final Map<String, String> data = {
-                              'username': username,
-                              'password': password,
-                            };
-
-                            try {
-                              final jsonData = json.encode(data);
-                              final response = await http.post(
-                                Uri.parse(url),
-                                headers: {'Content-Type': 'application/json'},
-                                body: jsonData,
-                              );
-
-                              if (response.statusCode == 200) {
-                                TokenManager.getInstance().setToken(response.body);
-                                UserManager.getInstance().setUsername(username);
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => MyApp()),
-                                );
-                              } else {
-                              debugPrint('Erreur lors de la requête. Code de statut: ${response.statusCode}\n${response.reasonPhrase}');
-                                return showDialog(context: context, builder: (context) {
-                                  return AlertDialog(
-                                    icon: Icon(Icons.error, color: Colors.red),
-                                    title: Text("Erreur ${response.statusCode}\n${response.reasonPhrase}"),
-                                    content: SingleChildScrollView(
-                                      child: ListBody(
-                                        children: <Widget>[
-                                          Text("Le serveur a rencontré une erreur et a renvoyé le message suivant :"),
-                                          Text(response.body)
-                                        ],
-                                      )
-                                    )
-                                  );
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height / 128),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: _isPasswordHidden,
+                      obscuringCharacter: "●",
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock, size: 48),
+                          suffixIcon: IconButton(
+                              icon: Icon(_isPasswordHidden ? Icons.visibility : Icons.visibility_off, size: 28),
+                              onPressed: () {
+                                setState(() {
+                                  _isPasswordHidden = !_isPasswordHidden;
                                 });
-                              }
-                            } catch (e) {
-                              debugPrint('Erreur lors de la requête: $e');
+                              },
+                          ),
+                          labelText: "Mot de passe",
+                          labelStyle: TextStyle(color: Colors.grey),
+                          floatingLabelStyle: TextStyle(color: Colors.purple, fontSize: 20),
+                          hintText: "●●●●●●",
+                          hintStyle: const TextStyle(color: Colors.grey, fontSize: 16),
+                          enabledBorder: const UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Colors.purple
+                              )
+                          )
+                      ),
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height / 128),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Row(
+                            children: [
+                              const Text(
+                                  "Se souvenir de moi",
+                                  style: TextStyle(
+                                      color: Colors.purple,
+                                      fontSize: 16
+                                  )),
+                              Checkbox(
+                                  value: _remindOfMe,
+                                  activeColor: Colors.purple,
+                                  onChanged: (changedState) {
+                                    setState(() {
+                                      _remindOfMe = changedState!;
+                                    });
+                                  }),
+                            ],
+                          )
+                        ),
+                    ElevatedButton(
+                        onPressed: () async {
+                          String username = _usernameController.text;
+                          String password = _passwordController.text;
+                          const url = 'https://api-ent.isenengineering.fr/v1/token';
+
+                          final Map<String, String> data = {
+                            'username': username,
+                            'password': password,
+                          };
+
+                          try {
+                            final jsonData = json.encode(data);
+                            final response = await http.post(
+                              Uri.parse(url),
+                              headers: {'Content-Type': 'application/json'},
+                              body: jsonData,
+                            );
+
+                            if (response.statusCode == 200) {
+                              TokenManager.getInstance().setToken(response.body);
+                              UserManager.getInstance().setUsername(username);
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => MyApp()),
+                              );
+                            } else {
+                            debugPrint('Erreur lors de la requête. Code de statut: ${response.statusCode}\n${response.reasonPhrase}');
+                              return showDialog(context: context, builder: (context) {
+                                return AlertDialog(
+                                  icon: Icon(Icons.error, color: Colors.red),
+                                  title: Text("Erreur ${response.statusCode}\n${response.reasonPhrase}"),
+                                  content: SingleChildScrollView(
+                                    child: ListBody(
+                                      children: <Widget>[
+                                        Text("Le serveur a rencontré une erreur et a renvoyé le message suivant :"),
+                                        Text(response.body)
+                                      ],
+                                    )
+                                  )
+                                );
+                              });
                             }
-                        },
-                        child: Text("Se connecter"))
-                    ],
-                  )
+                          } catch (e) {
+                            debugPrint('Erreur lors de la requête: $e');
+                          }
+                      },
+                      child: Text(
+                          "Se connecter",
+                          style: TextStyle(
+                            fontSize: 16
+                          ),
+                      ))
+                    ])
+                  ],
                 )
               )
-            ],
-          )
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 64)
+          ],
         )
       ),
       bottomSheet: Row(
@@ -181,13 +196,28 @@ class _LoginPageState extends State<LoginScreen> {
         crossAxisAlignment: CrossAxisAlignment.end,
 
         children: <Widget>[
-          Icon(Icons.lightbulb, size: 64),
-          const Text(
-              "Mentions légales",
-              style: TextStyle(
-                decoration: TextDecoration.underline
-              )),
-          Icon(Icons.hardware, size: 64)
+          const Image(
+              image: AssetImage('lib/assets/ISEN-Engineering-logo.png'),
+              width: 72,
+              height: 72
+          ),
+          TextButton(
+              onPressed: () {
+                //TODO: add Mentions légales screen
+                debugPrint("Mentions légales pressed");
+              },
+              child: const Text(
+                  "Mentions légales",
+                  style: TextStyle(
+                    fontSize: 12,
+                    decoration: TextDecoration.underline
+                  ))
+          ),
+          const Image(
+              image: AssetImage('lib/assets/ISEN-Repair-logo.png'),
+              width: 64,
+              height: 64
+          ),
         ],
       ),
     );
