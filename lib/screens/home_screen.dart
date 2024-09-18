1060e997d781002ad362a0700b2a06d21f88ac39
Pierre-Gei <pierregeiguer@gmail.com>
2024-09-18 16:02:06 +0200

    Next lesson V2
    Multiple day support

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ent/widgets/next_events.dart';
 import 'package:flutter/material.dart';
 import '../services/User_service.dart';
 import '../services/api_service.dart';
@@ -35,29 +36,29 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void updateCalendarEvents() {
     setState(() {
-      _calendarFuture = fetchEventsForDay(selectedDay);
+      _calendarFuture = fetchEventsForWeek(selectedDay);
     });
   }
 
-  Future<List<CalendarEvent>> fetchEventsForDay(DateTime day, {int maxDays = 30, int currentDayCount = 0}) async {
-    if (currentDayCount >= maxDays) {
-      return [];
-    }
+  Future<List<CalendarEvent>> fetchEventsForWeek(DateTime day, {int maxDays = 300, int currentDayCount = 0}) async {
+    List<CalendarEvent> events = [];
 
-    List<CalendarEvent> events = await apiService.fetchCalendar(
-      token,
-      DateTime(day.year, day.month, day.day),
-      DateTime(day.year, day.month, day.day, 23, 59)
-    ) as List<CalendarEvent>;
+    while (events.length < 2 && currentDayCount < maxDays) {
+      List<CalendarEvent> weekEvents = await apiService.fetchCalendar(
+        token,
+        DateTime(day.year, day.month, day.day, day.hour, day.minute),
+        DateTime(day.year, day.month, day.day, 23, 59).add(const Duration(days: 7)),
+      ) as List<CalendarEvent>;
 
-    if (events.length < 2 && currentDayCount < maxDays) {
-      // If no events for the current day, fetch for the next week and add to the current list
-      // Number of days to fetch is maxDays - currentDayCount
-      debugPrint('No events for $day, fetching for the next week');
+      events.addAll(weekEvents);
       currentDayCount += 7;
-      List<CalendarEvent> nextEvents = await fetchEventsForDay(day.add(const Duration(days: 1)), maxDays: maxDays, currentDayCount: currentDayCount + 1);
-      events.addAll(nextEvents);
+      day = day.add(const Duration(days: 7));
+      debugPrint('currentDayCount: $currentDayCount');
+      debugPrint('events.length: ${events.length}');
     }
+
+    // Ensure the events list is unique
+    events = events.toSet().toList();
     return events;
   }
 
@@ -104,8 +105,7 @@ class _HomeScreenState extends State<HomeScreen> {
               displayEvents = events.take(2).toList();
             }
 
-            return SingleChildScrollView(
-              child: Column(
+            return Column(
                 children: [
                   //large text
                   SizedBox(
@@ -121,10 +121,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ),
                   ),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 200,
-                    child: Container(
+                  Container(
                       child: Column(
                         children: [
                           Text(
@@ -132,17 +129,13 @@ class _HomeScreenState extends State<HomeScreen> {
                             style: Theme.of(context).textTheme.headlineMedium,
                             textAlign: TextAlign.left,
                           ),
-                          // Display current event + next one or next 2 events
-                          Expanded(
-                            child: DayView(
-                              date: selectedDay,
-                              onEventSelected: onEventSelected,
-                              events: displayEvents,
+                          SingleChildScrollView(
+                          child: Container(
+                            child: NextEvents(events: displayEvents),
                             ),
                           ),
                         ],
                       ),
-                    ),
                   ),
                   SizedBox(
                     width: double.infinity,
@@ -156,7 +149,6 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                           //place holder box
                           Container(
-                            height: 200,
                           ),
                         ],
                       ),
@@ -174,14 +166,12 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                           //place holder box
                           Container(
-                            height: 200,
                           ),
                         ],
                       ),
                     ),
                   ),
                 ],
-              ),
             );
           }
         },
---
+++ lib/widgets/next_events.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../model/calendar_event.dart';
+import 'day_view.dart';
+
+class NextEvents extends StatelessWidget {
+  final List<CalendarEvent> events;
+
+  const NextEvents({Key? key, required this.events}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Group events by day
+    Map<DateTime, List<CalendarEvent>> groupedEvents = {};
+    for (var event in events) {
+      DateTime eventDate = DateTime(event.start.year, event.start.month, event.start.day);
+      if (!groupedEvents.containsKey(eventDate)) {
+        groupedEvents[eventDate] = [];
+      }
+      groupedEvents[eventDate]!.add(event);
+    }
+
+    // Convert the map to a list of date and events pairs
+    List<MapEntry<DateTime, List<CalendarEvent>>> groupedEventsList = groupedEvents.entries.toList();
+
+    return Column(
+      children: groupedEventsList.map((entry) {
+        DateTime date = entry.key;
+        List<CalendarEvent> dayEvents = entry.value;
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 2),
+              child: Text(
+                DateFormat('E, dd MMM yyyy').format(date),
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+            Container(
+              constraints: BoxConstraints(
+                maxHeight: MediaQuery.of(context).size.height/5, // Adjust the height as needed
+              ),
+              child: DayView(date: date, events: dayEvents, onEventSelected: (event) {
+                Navigator.pushNamed(context, '/event', arguments: event);
+              }),
+            ),
+          ],
+        );
+      }).toList(),
+    );
+  }
+}
