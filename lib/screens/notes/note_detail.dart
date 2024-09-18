7fb506401aa6c27c3c73b8a5a49eaff476e28432
Maxou <Jackson.Kyoshi@gmail.com>
2024-09-18 12:52:14 +0200

    feat: pretty notations details

--- lib/screens/notes/note_detail.dart
+++ lib/screens/notes/note_detail.dart
@@ -10,74 +10,154 @@ class NoteDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ApiService apiService = ApiService('https://api-ent.isenengineering.fr');
+    final ApiService apiService =
+        ApiService('https://api-ent.isenengineering.fr');
 
     return FutureBuilder<NotationClass>(
       future: _fetchNotationClass(code, apiService),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return AlertDialog(
-            title: Text('Loading...'),
-            content: CircularProgressIndicator(),
+          return Center(
+            child: CircularProgressIndicator(),
           );
         } else if (snapshot.hasError) {
-          return AlertDialog(
-            title: Text('Error'),
-            content: Text('Failed to load notation data: ${snapshot.error}'),
-            actions: <Widget>[
-              TextButton(
-                child: Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
+          return Center(
+            child: Text('Failed to load notation data: ${snapshot.error}'),
           );
         } else if (snapshot.hasData) {
-          NotationClass notation = snapshot.data!;
-          return AlertDialog(
-            title: Text('Informations'),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text('Matière: ${notation.code}'),
-                Text('Note personnelle: ${notation.notePersonal}'),
-                Text('Moyenne de classe: ${notation.noteAverage}'),
-                Text('Note minimale: ${notation.noteMin}'),
-                Text('Note maximale: ${notation.noteMax}'),
-                Text("Nombre d'étudiants: ${notation.presence}"),
-              ],
-            ),
-            actions: <Widget>[
-              TextButton(
-                child: Text('Cancel'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
+          // Une fois les données récupérées, afficher la feuille modale
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            _showNotationModal(context, snapshot.data!).then((_) {
+              Navigator.pop(
+                  context); // Fermer la page une fois la modale fermée
+            });
+          });
+
+          return SizedBox
+              .shrink(); // N'affiche rien pendant que la feuille modale est affichée
         } else {
-          return AlertDialog(
-            title: Text('No Data'),
-            content: Text('No data available for the selected notation.'),
-            actions: <Widget>[
-              TextButton(
-                child: Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
+          return Center(
+            child: Text('No data available for the selected notation.'),
           );
         }
       },
     );
   }
 
-  Future<NotationClass> _fetchNotationClass(String code, ApiService apiService) async {
+  Future<NotationClass> _fetchNotationClass(
+      String code, ApiService apiService) async {
     String token = TokenManager.getInstance().getToken();
     List<NotationClass> notations = await apiService.fetchNotationsClass(token);
     return notations.firstWhere((notation) => notation.name == code);
   }
+
+  // Modification pour gérer la fermeture correcte de la modale
+  Future<void> _showNotationModal(
+      BuildContext context, NotationClass notation) {
+    return showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(25.0),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      builder: (BuildContext context) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: Container(
+                  height: 5,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: double.infinity,
+                height: 100,
+                child: Text(
+                  notation.code,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.school),
+                          SizedBox(width: 8),
+                          Text(
+                            'Note élève :',
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        notation.notePersonal,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              buildRowWithIcon(Icons.assessment, 'Moyenne classe :', notation.noteAverage),
+              buildRowWithIcon(Icons.trending_up, 'Note maximale :', notation.noteMax),
+              buildRowWithIcon(Icons.trending_down, 'Note minimale :', notation.noteMin),
+              buildRowWithIcon(Icons.people, "Nombre d'étudiant :", notation.presence),
+
+              ],
+          )
+        );
+      },
+    );
+  }
 }
+
+Widget buildRowWithIcon(IconData icon, String text, String value) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Row(
+        children: [
+          Icon(icon, color: Colors.black),
+          SizedBox(width: 8),
+          Text(
+            text,
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+      Text(
+        value,
+        style: TextStyle(
+          fontSize: 16,
+        ),
+      ),
+    ],
+  );
+}
+
