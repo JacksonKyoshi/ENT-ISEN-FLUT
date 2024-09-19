0de9f8da1dfe83661fab4669297dc7b48734f06f
Sigeth <52868615+Sigeth@users.noreply.github.com>
2024-09-19 12:28:08 +0200

    chore: changing org and name of the android app

--- android/app/src/main/kotlin/com/example/ent/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ent
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/fr/isenengineering/flut/MainActivity.kt
+++ android/app/src/main/kotlin/fr/isenengineering/flut/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flut
+package fr.isenengineering.flut
 
 import io.flutter.embedding.android.FlutterActivity
 
