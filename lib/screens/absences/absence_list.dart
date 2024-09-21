0050dd9302ed8d87e270d10a819875aec5ad9601
Sigeth <52868615+Sigeth@users.noreply.github.com>
2024-09-21 13:40:44 +0200

    fix(absences): can reload when there are absences

--- lib/screens/absences/absence_list.dart
+++ lib/screens/absences/absence_list.dart
@@ -36,49 +36,52 @@ class AbsenceList extends StatelessWidget {
       );
     }
 
-    return ListView.builder(
-      itemCount: absences.length,
-      itemBuilder: (context, index) {
-        Absence absence = absences[index];
-        return CustomCardList(
-          constraints: BoxConstraints(maxHeight: 110),
-          leadingColor: defineAbsenceColor(absence),
-          leading: Text(
-            absence.date.substring(0, absence.date.lastIndexOf("/")),
-            style: TextStyle(
-                fontSize: 14.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black),
-          ),
-          title: AutoSizeText(
-            absence.course,
-            style: TextStyle(fontSize: 20.0,fontWeight:FontWeight.bold),
-            overflow: TextOverflow.ellipsis,
-            maxLines: 1,
-          ),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                absence.hours.replaceAll(":", "h"),
-                style: TextStyle(
-                    fontSize: 14.0, color: Color(0xFF555555)),
-              ),
-              Text(
-                absence.reason,
-                style: TextStyle(
-                    fontSize: 14.0, color: defineAbsenceColor(absence)),
-              )
-            ]
-          ),
-          trailing: Text(
-            absence.duration.replaceAll(":", "h"),
-            style: TextStyle(
-                fontSize: 24.0, fontWeight: FontWeight.bold, color: defineAbsenceColor(absence)),
-          ),
-          onTap: () {  },
-        );
-      },
+    return RefreshIndicator(
+        onRefresh: onRefresh,
+        child: ListView.builder(
+        itemCount: absences.length,
+        itemBuilder: (context, index) {
+          Absence absence = absences[index];
+          return CustomCardList(
+            constraints: BoxConstraints(maxHeight: 110),
+            leadingColor: defineAbsenceColor(absence),
+            leading: Text(
+              absence.date.substring(0, absence.date.lastIndexOf("/")),
+              style: TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+            title: AutoSizeText(
+              absence.course,
+              style: TextStyle(fontSize: 20.0,fontWeight:FontWeight.bold),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  absence.hours.replaceAll(":", "h"),
+                  style: TextStyle(
+                      fontSize: 14.0, color: Color(0xFF555555)),
+                ),
+                Text(
+                  absence.reason,
+                  style: TextStyle(
+                      fontSize: 14.0, color: defineAbsenceColor(absence)),
+                )
+              ]
+            ),
+            trailing: Text(
+              absence.duration.replaceAll(":", "h"),
+              style: TextStyle(
+                  fontSize: 24.0, fontWeight: FontWeight.bold, color: defineAbsenceColor(absence)),
+            ),
+            onTap: () {  },
+          );
+        },
+      )
     );
   }
 }
